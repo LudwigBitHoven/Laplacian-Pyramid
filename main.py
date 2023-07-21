5e74316345aa01ab7a77573ed5446dae53ee729d
Николай Чаптыков <nick.chaptykov@gmail.com>
2023-07-21 22:12:34 +0300

    Getting ready for deploy

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from img import ImgWidget
 from kivy.graphics.texture import Texture
 from kivy.core.image import Image as CoreImage
 from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy import platform
+from io import BytesIO
 import numpy as np
 import os
+
+from functools import partial
 
 
 class MainLayout(GridLayout):
@@ -26,6 +31,12 @@ class Laplas(MDApp):
         self.theme_cls.material_style = "M2"
         return self.root
 
+    def on_start(self):
+        if platform == "android":
+            from android.permissions import Permission, request_permissions
+            from jnius import autoclass
+            request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+
     def load_kvs(self):
         Builder.load_file('kv/data.kv')
         Builder.load_file('kv/img_widgets.kv')
@@ -33,24 +44,38 @@ class Laplas(MDApp):
     def setdir(self):
         os.chdir(self.path)
 
-    def BuildPyramid(self):
+    def TriggerBuilding(self):
+        self.root.ids['result_scroll'].clear_widgets()
+        Clock.schedule_once(self.BuildPyramid)
+
+    def BuildPyramid(self, *args):
         path = self.root.ids['img_frame'].ids['img'].source
-        img = self.processor.open_img(path)
-        from io import BytesIO
-        result = self.processor.lap_pyramid(img)
+        start_img = self.processor.open_img(path)
+        result = self.processor.lap_pyramid(start_img)
+        Clock.schedule_once(partial(self.DrawPyramid, result), 0)
+
+    def DrawPyramid(self, result, *args):
         result = self.processor.img_to_byte(result)
         w, h, rgb = result[0].shape
         for img in result:
-            buf = bytes(img)
-            texture = Texture.create(size=(h, w))
-            texture.blit_buffer(buf, colorfmt='rgb')
-            texture.flip_vertical()
-            temp = Image()
-            temp.texture = CoreImage(texture).texture
-            temp.texture = temp.texture
-            temp2 = ImgWidget()
-            temp2.add_widget(temp)
-            self.root.ids['result_scroll'].add_widget(temp2)
+            Clock.schedule_once(partial(self.DrawWidget, img, w, h), 0.5)
+
+    def DrawWidget(self, img, w, h, p):
+        buf = bytes(img)
+        texture = Texture.create(size=(h, w))
+        texture.blit_buffer(buf, colorfmt='rgb')
+        texture.flip_vertical()
+        temp = Image()
+        temp.texture = CoreImage(texture).texture
+        temp2 = ImgWidget()
+        temp2.add_widget(temp)
+        self.root.ids['result_scroll'].add_widget(temp2)
+
+    def SaveImages(self):
+        content = self.root.ids['result_scroll'].children
+        for i, j in enumerate(content):
+            j.export_to_png(f"laplacian/{i}_laplacian.png")
+
 
 if __name__ == '__main__':
     Laplas().run()
--- processor.py
+++ processor.py
@@ -4,11 +4,19 @@ from skimage.io import imread, imshow
 from skimage import img_as_float
 from scipy.signal import convolve2d
 from numpy.fft import fft2, fftshift
-import io
 
 class ImgProcessor():
     def __init__(self):
-        pass
+        self.sigma = 0.33
+        self.n_layers = 4
+
+    def set_params(self, sigma, n_layers):
+        if sigma and n_layers:
+            self.sigma = float(sigma)
+            self.n_layers = int(n_layers)
+        else:
+            self.sigma = 0.33
+            self.n_layers = 4
 
     def conv(self, img, kernel):
         r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
@@ -19,22 +27,22 @@ class ImgProcessor():
     def gauss(self, x, y, sigma):
         return (1 / ((2 * pi) * sigma**2) * e**((- x**2 - y**2) / (2 * sigma**2)))
 
-    def gauss_pyramid(self, img, sigma, n_layers):
+    def gauss_pyramid(self, img):
         result = []
-        k = int(round(6 * sigma + 1))
-        core = np.array([[self.gauss(x, y, sigma) for x in range(-k, k + 1)] for y in range(-k, k + 1)])
+        k = int(round(6 * self.sigma + 1))
+        core = np.array([[self.gauss(x, y, self.sigma) for x in range(-k, k + 1)] for y in range(-k, k + 1)])
         kernel = core / np.sum(core, axis=(0, 1))
-        for i in range(n_layers):
+        for i in range(self.n_layers):
             img = self.conv(img, kernel)
             img = np.clip(img, 0, 1)
             result.append(img)
         return np.array(result)
 
-    def lap_pyramid(self, img, sigma = 0.66, n_layers = 6):
+    def lap_pyramid(self, img):
         g_img = [img[:, :, :3]]
-        g = self.gauss_pyramid(img, sigma, n_layers)
+        g = self.gauss_pyramid(img)
         result = []
-        for i in range(n_layers):
+        for i in range(self.n_layers):
             g_img.append(g[i])
             pic = g_img[i] - g_img[i + 1]
             result.append(pic)
